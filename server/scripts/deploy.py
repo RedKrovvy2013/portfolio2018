9a8f2da8a5212c130168e6bef301edb6ab44b3f1
mikecirone <michael@ballparq.io>
2018-03-06 16:19:16 -0800

    deployment dev'ing

---
+++ server/scripts/deploy.py
@@ -0,0 +1,89 @@
+from shutil import copyfile, rmtree
+import os
+import sys
+from subprocess import Popen, PIPE, STDOUT
+
+deployFlag = sys.argv[1]
+
+if deployFlag=='a':
+    dirname = 'portfolio_a'
+    otherDirname = 'portfolio_b'
+elif deployFlag=='b':
+    dirname = 'portfolio_b'
+    otherDirname = 'portfolio_a'
+else:
+    sys.exit()
+
+cmdDir = os.path.join(os.getcwd(), "portfolio_"+deployFlag)
+
+cmds = [
+         "npm install",
+         "gulp sass"
+       ]
+
+logString = ""
+for fullCmd in cmds:
+    logString += "\n\n"
+    logString += fullCmd
+    logString += "\n-----------------------------------\n"
+
+    cmd = fullCmd.split()
+    p = Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1, cwd=cmdDir)
+    with p.stdout:
+        for line in iter(p.stdout.readline, b''):
+            print line,  #NOTE: the comma prevents duplicate newlines (softspace hack)
+            logString += line
+    p.wait()
+
+logfile = open("log.txt", "w")
+logfile.write(logString)
+logfile.close()
+
+response = raw_input("Continue? (y)")
+if response!='y':
+   sys.exit()
+
+logString = ""
+logString += "\n\n"
+logString += "stopping other server"
+logString += "\n-----------------------------------\n"
+
+cmds = [
+         "forever stop " + os.path.join(os.getcwd(),otherDirname,'server/server.js')
+       ]
+
+for fullCmd in cmds:
+    logString += "\n\n"
+    logString += fullCmd
+    logString += "\n-----------------------------------\n"
+
+    cmd = fullCmd.split()
+    p = Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1)
+    with p.stdout:
+        for line in iter(p.stdout.readline, b''):
+            print line,
+            logString += line
+    p.wait()
+
+logfile = open("log.txt", "a")
+logfile.write(logString)
+
+
+rmtree(os.path.join(os.getcwd(),otherDirname))
+
+cmds = [
+         "forever start -o outTest.log -e errTest.log " + os.path.join(os.getcwd(),dirname,'server/server.js 80')
+       ]
+
+for fullCmd in cmds:
+    logString += "\n\n"
+    logString += fullCmd
+    logString += "\n-----------------------------------\n"
+
+    cmd = fullCmd.split()
+    p = Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1)
+    with p.stdout:
+        for line in iter(p.stdout.readline, b''):
+            print line,
+            logString += line
+    p.wait()
--- server/server.js
+++ server/server.js
@@ -5,5 +5,5 @@ var app = express()
 
 app.use(express.static(path.join(__dirname, '../client/public')))
 
-const port = 8080
+const port = process.argv[2] ? process.argv[2] : 8080
 const server = app.listen(port, function() {})
