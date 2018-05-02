ebba3ec9ab8abd44ca8eb20edb8363802891bf39
mikecirone <michael@ballparq.io>
2018-05-01 18:30:55 -0700

    added connect 4 project to portfolio

--- server/server.js
+++ server/server.js
@@ -5,5 +5,7 @@ var app = express()
 
 app.use(express.static(path.join(__dirname, '../client/public')))
 
+app.use('/connect4', express.static(path.join(__dirname, '../client/connect4')))
+
 const port = process.argv[2] ? process.argv[2] : 8080
 const server = app.listen(port, function() {})
