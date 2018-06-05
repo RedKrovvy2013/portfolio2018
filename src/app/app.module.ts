4edba2c9ac1133804da4552a83babe74d64107ee
mikecirone <michael@ballparq.io>
2018-06-04 22:58:26 -0700

    detail components: heavy lifting done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UiComponent } from './ui-module/ui.component';
+import { DetailsComponent } from './details/details.component';
+import { DetailsInnerComponent } from './details/details-inner/details-inner.component';
+
+const appRoutes: Routes = [
+    { path: 'details/:name', component: DetailsComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UiComponent,
+    DetailsComponent,
+    DetailsInnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/details/details-inner/details-inner.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-inner',
+  templateUrl: './details-inner.component.html',
+  styleUrls: ['./details-inner.component.scss']
+})
+export class DetailsInnerComponent implements OnInit {
+
+  header: string
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
